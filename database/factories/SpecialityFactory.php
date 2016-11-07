e67df819355b35d468bb19082e90ef72c89213e8
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-11-07 13:40:11 +0100

    Sync

--- database/factories/SpecialityFactory.php
+++ database/factories/SpecialityFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(Scool\Curriculum\Models\Speciality::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->word,
+        'code' => $faker->numberBetween(500,699)
     ];
 });
--- src/Models/Module.php
+++ src/Models/Module.php
@@ -3,6 +3,7 @@
 namespace Scool\Curriculum\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Scool\Curriculum\Traits\HasSubmodules;
 
 class Module extends Model
 {
